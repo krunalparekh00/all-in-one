20d54ad73ccfe32418562c608ac08c16e6c894e6
ankushnirwan1 <133894685+ankushnirwan1@users.noreply.github.com>
2023-06-26 14:36:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = ""
-    API_ID = 
-    API_HASH = ""
-    AUTH_USERS = ''
+    BOT_TOKEN = "6147806383:AAET9lLN9Xa6kYgsdvqYxWgrlMbT64RrvBw"
+    API_ID = "23321997"
+    API_HASH = "3c0378a72f84d4dfe7d701bba5f3bbaa"
+    AUTH_USERS = "5903548785"
 
